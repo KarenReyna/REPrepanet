505a2ab3bc40948f31e6b00ab27b55b6fd71911f
KarenReyna <karen.reyna95@gmail.com>
2017-09-30 16:48:30 -0500

    Add Home View V1

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,37 @@
 import * as React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+const style = {
+  height: 400,
+};
+
 export class Home extends React.Component<any, any> {
   public render() {
-    return <RaisedButton label = "Login" onClick={this.props.loginClicked}/>
+    return (
+      <div>
+        <RaisedButton label = "Login" style={{textAlign: 'right'}} onClick={this.props.loginClicked}/>
+        <Card>
+          <CardHeader
+          />
+          <CardMedia
+            overlay={<CardTitle title="Recursos Educativos Prepanet" subtitle="Explora diversos materiales educativos y mejora tu forma de aprender." />}
+          >
+            <img src="/src/assets/home.png" style={style}/>
+          </CardMedia>
+          <CardTitle title="Colecciones" subtitle="Escoge sobre qué materia quieres consultar." />
+          <CardActions>
+            <RaisedButton label="Química" />
+            <RaisedButton label="Física" />
+          </CardActions>
+
+          <CardTitle title="Recursos Populares" subtitle="Recursos preferidos." />
+          <CardActions>
+            <RaisedButton label="Química" />
+            <RaisedButton label="Física" />
+          </CardActions>
+        </Card>
+      </div>
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,31 @@ import * as ReactDOM from 'react-dom';
 import { AppContainer } from "react-hot-loader";
 import { Provider } from 'react-redux';
 import Main from './containers/main';
-import { MuiThemeProvider } from "material-ui/styles";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
 import registerServiceWorker from './scripts/registerServiceWorker';
 import configureStore from './store'
 
 const store = configureStore();
 
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/colors
+const muiTheme = getMuiTheme({
+  palette: {
+    //textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
+
 const rootEl = document.getElementById("root");
 ReactDOM.render(
   <AppContainer>
     <Provider store={store}>
-      <MuiThemeProvider>
+      <MuiThemeProvider  muiTheme={muiTheme}>
         <Main />
       </MuiThemeProvider>
     </Provider>
