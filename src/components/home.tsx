7395c3f0f0150de3924f3f56b7c2c55e559cc177
Fabian Montemayor <f.montemayor26@gmail.com>
2017-10-12 10:59:27 -0500

    Name change

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -43,7 +43,7 @@ export class Home extends React.Component<any, any> {
       <div>
         <RaisedButton label = "Login" style={{textAlign: 'right', float: 'right'}} onClick={this.props.loginClicked}/>
         <RaisedButton label = "Registro" style={{textAlign: 'right', float: 'right'}} onClick={this.props.registerClicked}/>
-        <RaisedButton label = "Create Item" style={{textAlign: 'right', float: 'right'}} onClick={this.props.createClicked}/>
+        <RaisedButton label = "New Item" style={{textAlign: 'right', float: 'right'}} onClick={this.props.createClicked}/>
         <Card>
           <CardHeader/>
           <CardMedia
