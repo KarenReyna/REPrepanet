30edc11d99894b525dcbf5e8fefb0466626e9342
Fabian Montemayor <f.montemayor26@gmail.com>
2017-10-17 07:07:42 -0500

    resource model

--- src/api/models/resource.ts
+++ src/api/models/resource.ts
@@ -1,2 +1,24 @@
 import { Schema, model, Document, Model } from 'mongoose';
 
+interface IResourceDocument extends Document {
+    name: string,
+    description: string,
+    url: string,
+    image: string, // CHECK
+    tags: string[],
+}
+
+interface IResource extends IResourceDocument {}
+interface IResourceModel extends Model<IResource> {}
+
+var ResourceSchema = new Schema({
+    name: String,
+    description: String,
+    url: String,
+    image: String,
+    tags: Array<String>()
+});
+
+var Resource: IResourceModel = model<IResource, IResourceModel>('User', ResourceSchema);
+
+export { Resource, IResource, IResourceModel, IResourceDocument };
