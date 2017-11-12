fe3b77300b8761b71fe507d6fc76cfdd94b1a799
KarenReyna <karen.reyna95@gmail.com>
2017-11-11 20:26:29 -0600

    Delete display of banner

--- src/frontend/presentational/components/home.tsx
+++ src/frontend/presentational/components/home.tsx
@@ -10,23 +10,7 @@ import Styles from 'Presentational/style/elementStyles';
 import { Status } from 'Config/constants';
 import { LinearProgress } from 'material-ui/Progress';
 
-// function createContent(object) {
-//   switch(object.status) {
-//     case Status.Ready:
-//       if(object && object.all && object.all.length > 0)
-//         return object.all.map((category) => (
-//           <CollectionCard key={object.all._id} title={category.title} />
-//         ));
-//       return (<p>No hay categorías</p>);
-//     case Status.Failed:
-//       return (<p>No hay conexión a Internet</p>);
-//     case Status.WaitingOnServer:
-//     default:
-//       return (<LinearProgress mode="indeterminate" />);
-//   }
-// }
-
-function createContentV2(object, resources) {
+function createContent(object, resources) {
   if(resources.all != null && resources.all.length > 0)
     console.log(resources.all);
   switch(object.status) {
@@ -46,8 +30,7 @@ function createContentV2(object, resources) {
 
 export class Home extends React.Component<any, any> {
   public render() {
-    let categoryContent = createContentV2(this.props.categories, this.props.resources);
-    // let resourceContent = createContent(this.props.resources);
+    let categoryContent = createContent(this.props.categories, this.props.resources);
     
     return (
       <div>
@@ -55,22 +38,9 @@ export class Home extends React.Component<any, any> {
           <NavButton label="Entrar" onClick={this.props.loginShow}/>
         </Navbar>
 
-        <Container bgColor={Styles.colors.green}>
-          <br />
-          <h2 style={{color: Styles.colors.white}}>Recursos Educativos Prepanet</h2>
-          <p style={{color: Styles.colors.white}}>Explora diversos materiales educativos y mejora tu forma de aprender.</p>
-          {/* <Button label="Explorar"/> */}
-          <br />
-        </Container>
-
         <Container>
-          <h3>Categorías</h3>
             {categoryContent}
         </Container>
-        {/* <Container>
-          <h3>Recursos populares</h3>
-            {resourceContent}
-        </Container> */}
       </div>
     );
   }
--- src/frontend/presentational/elements/CollectionCardV2.tsx
+++ src/frontend/presentational/elements/CollectionCardV2.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import Styles from 'Presentational/style/elementStyles';
-// import PropTypes from 'prop-types';
-//import {GridList, GridListTile} from 'material-ui/GridList';
-//  import { Resource } from 'Config/constants';
 
 export default class CollectionCardV2 extends React.Component<any, any> {
-    // static propTypes = {
-    //     title: PropTypes.string.isRequired,
-    // }
     public render() {
       return(
         <div>
